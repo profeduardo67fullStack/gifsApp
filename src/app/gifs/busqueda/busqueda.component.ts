8e2e1668070a64742304ce2118daf9300a37993e
ProfEduardo <profeduardo67.cap@gmail.com>
2022-01-03 16:15:50 -0600

    Se crea logica para el cuadro de busqueda y su limpieza
    Se crea el código par que se tome lo que se escribe en el cuadro de textos, y que se limpie al dar enter

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent{
+ // buscar(event: KeyboardEvent){//El tipo de evento cuando se pulsa una tecla
 
-  constructor() { }
+ //Para solucionar el que se limpie la caja
+ //utilizaremos el decorador @ViewChild()
 
-  ngOnInit(): void {
+ //Con el símbolo de cierre de admiración, le decimos al Angular
+ //que si existe el elemento y que no será nulo
+ //Not-null assertion operator (!)
+ @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+    buscar(){
+      const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value='';
   }
-
 }
