34d680e38b28d3f91164844bb7fc2dcb5ac01da4
ProfEduardo <profeduardo67.cap@gmail.com>
2022-01-11 12:51:22 -0600

    Se crea lógica para agregar items, en el sidebar
    Se hace una inyeccion del servicio, y se crea propiedad para poner ngFor en el HTML

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { //Decalrando una variable en el constructora hacemos la inyección de dependencias
+  //Estamos inyectando el servicio
 
-  ngOnInit(): void {
   }
+
+  get historial(){//Creamos una propiedad getter, para poder modificar el HTML
+    return this.gifsService.historial;
+  }
+ 
+  
 
 }
